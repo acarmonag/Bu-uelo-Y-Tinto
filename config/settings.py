f1e320f00da1ddd8c9b0810907bacbb5b6d6e748
Juan Pablo Cortes <jpcortesg@eafit.edu.co>
2025-03-28 21:54:20 -0500

    Cors

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,29 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# Configuración de CORS
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # Configurar el modelo de usuario personalizado
 AUTH_USER_MODEL = 'infrastructure.User'
 
@@ -71,6 +94,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,7 +102,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'src.middleware.GlobalCurrentRequestMiddleware',
 ]
 
